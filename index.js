69c475d13984c8b9d85c4d1b89a3f4cf8ec5603f
azeezabass2005 <azeezabass2005@gmail.com>
2024-12-20 16:53:54 +0100

    chore: completed

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5173', // Your frontend URL
+  origin: '*', // Your frontend URL
   credentials: true
 }));
 app.use(express.json());
